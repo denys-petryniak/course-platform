b68f074f114bf2c92a5519f2ba7f0ff451a597b5
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-24 20:50:30 +0200

    Create responsive videos

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -8,14 +8,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <iframe
-    class="max-w-[100%]"
-    width="560"
-    height="315"
-    :src="`https://player.vimeo.com/video/${props.videoId}`"
-    title="Video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-  ></iframe>
+  <div class="aspect-video">
+    <iframe
+      class="w-full h-full"
+      :src="`https://player.vimeo.com/video/${props.videoId}`"
+      title="Video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
