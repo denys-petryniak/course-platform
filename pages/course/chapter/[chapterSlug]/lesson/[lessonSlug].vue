3f44bb505d6223d8f5ce5808c0d360e65c95892f
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-24 15:45:42 +0200

    Add meta title to lesson page

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -13,6 +13,12 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
+
+const title = computed(() => `${lesson.value?.title} | ${course.title}`);
+
+useHead({
+  title,
+});
 </script>
 
 <template>
