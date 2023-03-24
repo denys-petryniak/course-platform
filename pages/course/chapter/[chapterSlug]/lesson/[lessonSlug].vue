40ac753997b19c81205095ff782a4742dfd62d7e
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-24 16:51:56 +0200

    Track progress with useState composable

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 const course = useCourse();
 const route = useRoute();
 
@@ -19,6 +19,31 @@ const title = computed(() => `${lesson.value?.title} | ${course.title}`);
 useHead({
   title,
 });
+
+const progress = useState("progress", () => {
+  return [];
+});
+
+const isLessonComplete = computed(() => {
+  if (!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
+
+  if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+    return false;
+  }
+
+  return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+});
+
+const toggleComplete = () => {
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
+
+  progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+    !isLessonComplete.value;
+};
 </script>
 
 <template>
@@ -45,5 +70,9 @@ useHead({
     </div>
     <VideoPlayer v-if="lesson?.videoId" :videoId="lesson.videoId" />
     <p>{{ lesson?.text }}</p>
+    <LessonCompleteButton
+      :modelValue="isLessonComplete"
+      @update:modelValue="toggleComplete"
+    />
   </div>
 </template>
