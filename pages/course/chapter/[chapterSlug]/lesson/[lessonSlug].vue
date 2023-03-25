d000231383994e6ba4bf3cd528b58cdae124a3eb
Denys Petryniak <denys.petryniak@gmail.com>
2023-03-25 04:14:26 +0200

    Add types to useCourse composable

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -47,27 +47,27 @@ const toggleComplete = () => {
 <template>
   <div>
     <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-      Lesson {{ chapter?.number }} - {{ lesson?.number }}
+      Lesson {{ chapter.number }} - {{ lesson.number }}
     </p>
     <h2 class="my-0">{{ lesson?.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
       <NuxtLink
-        v-if="lesson?.sourceUrl"
+        v-if="lesson.sourceUrl"
         class="font-normal text-md text-gray-500"
         :href="lesson.sourceUrl"
       >
         Download Source Code
       </NuxtLink>
       <NuxtLink
-        v-if="lesson?.downloadUrl"
+        v-if="lesson.downloadUrl"
         class="font-normal text-md text-gray-500"
         :href="lesson.downloadUrl"
       >
         Download Video
       </NuxtLink>
     </div>
-    <VideoPlayer v-if="lesson?.videoId" :videoId="lesson.videoId" />
-    <p>{{ lesson?.text }}</p>
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
     <LessonCompleteButton
       :modelValue="isLessonComplete"
       @update:modelValue="toggleComplete"
