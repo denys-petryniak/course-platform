34fd75b8d88f784d50aaa8bef83b68f2df7e9dd3
Denys Petryniak <denys.petryniak@gmail.com>
2023-04-01 14:07:08 +0300

    Create useFetchWithCache composable

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,14 +1,6 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
-  const { data, error } = await useFetch(
+import { LessonWithPath } from "~~/types/course";
+
+export default async (chapterSlug: string, lessonSlug: string) =>
+  useFetchWithCache<LessonWithPath>(
     `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
   );
-
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
-  }
-
-  return data;
-};
