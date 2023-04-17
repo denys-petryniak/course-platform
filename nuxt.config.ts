17ea15add15ac7149f958245c776c00272d3e507
Denys Petryniak <denys.petryniak@gmail.com>
2023-04-17 16:16:38 +0300

    Manage static assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
+import vsharp from "vite-plugin-vsharp";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: ["/landing"],
     },
+  },
+
+  vite: {
+    plugins: [vsharp()],
   },
 
   modules: [
